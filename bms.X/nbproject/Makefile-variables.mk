01d406b50d5b3dfd3b2ce0c44258aaadf890d01b
Khang Nguyen <nnmkhang166@gmail.com>
2018-02-02 17:18:28 -0500

    Test for turning on an LED

---
+++ bms.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=bms.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/bms.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=bms.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/bms.x.tar
